1c473d52e4419b6ff997cf63d43f3babee9e910b
jirklym <jirklym@penguin>
2022-12-13 15:53:36 +0100

    uprava dle domluvy

--- program.php
+++ program.php
@@ -1,41 +1,72 @@
 <?php
+
 declare(strict_types=1);
+$counter = 0;
 
-$stats = array(
-    (object)["filter"=>'/test\.N/',"count"=>0,"key"=>"notice"],
-    (object)["filter"=>'/test\.I/',"count"=>0,"key"=>"info"],
-    (object)["filter"=>'/test\.EM/',"count"=>0,"key"=>"energency"],
-    (object)["filter"=>'/test\.W/',"count"=>0,"key"=>"warning"],
-    (object)["filter"=>'/test\.ER/',"count"=>0,"key"=>"error"],
-    (object)["filter"=>'/test\.A/',"count"=>0,"key"=>"alert"],
-);
+class Stat
+{
+    public static $stats = [];
 
-$file_lines = function(string $filename) {
+    public $count;
+    public $patern;
+    public $identifier;
+
+    function __construct(string $patern, string $identifier)
+    {
+        $this->count = 1;
+        $this->pattern = $patern;
+        $this->identifier = $identifier;
+    }
+
+    public static function lineCheck(string $line)
+    {
+
+        foreach (Stat::$stats as $stat) {
+            if (preg_match($stat->pattern, $line)) {
+                $stat->increment();
+                if (!preg_match("/test.DEBUG/", $line)) {
+                    return $line;
+                }
+            }
+        }
+        //not found add a new stat
+        $pattern = '/test.' . substr($line, 27, 4) . "/";
+        $id = substr($line, 27, 6);
+        array_push(Stat::$stats, new Stat($pattern, $id));
+        if (!preg_match("/test.DEBUG/", $line)) {
+            return $line;
+        }
+    }
+
+    public function increment()
+    {
+        $this->count++;
+    }
+}
+
+
+$file_lines = function (string $filename) {
     $file = fopen($filename, 'r');
     while (($line = fgets($file)) !== false) {
+        usleep(30000);
         yield $line;
     }
     fclose($file);
 };
 
-foreach($file_lines($argv[1]) as $line)
-{
-    foreach($stats as $stat)
-    {
-        if(preg_match($stat->filter,$line))
-        {
-            $stat->count++;
-        }
-        
+foreach ($file_lines($argv[1]) as $line) {
+    echo (Stat::lineCheck($line));
+    $counter++;
+    if ($counter > 30) {
+        call_user_func('writeOutputCall', Stat::$stats);
+        $counter = 0;
     }
-    call_user_func('writeOutputCall',$stats);
 }
+
 
 function writeOutputCall($stats)
 {
-    foreach($stats as $stat){
-        echo $stat->key . $stat->count . PHP_EOL;
+    foreach ($stats as $stat) {
+        echo $stat->identifier . " " . $stat->pattern . " "  . $stat->count . PHP_EOL;
     }
-    system("clear");
 }
-
